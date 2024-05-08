546b3b4f64fb55046259dc365859032eff0eebce
gregdenay <gregoire.denay@cvua-rrw.de>
2024-05-08 13:13:07 +0200

    bump validate

--- workflow/envs/git_workflow.post-deploy.sh
+++ workflow/envs/git_workflow.post-deploy.sh
@@ -19,7 +19,7 @@ deploy () {
 repo="https://github.com/NRW-GEUBT/geuebt-validate"
 
 # Commit hash to use
-tag="0.2.5"
+tag="0.2.6"
 
 # Local directory to save the Repo
 local_dir="$HOME/.nrw-geuebt/geuebt-validate/"
