6b71482a6004102be3484deba633c37503bda56c
gregdenay <gregoire.denay@cvua-rrw.de>
2026-01-26 15:22:10 +0100

    fix busco db

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+### 1.5.1 
+
+fix busco DB
+
 ### 1.5.0
 
 Bump validate to 1.50 with new kraken2 and busco databases
--- workflow/envs/validate.post-deploy.sh
+++ workflow/envs/validate.post-deploy.sh
@@ -2,7 +2,7 @@
 set -Eeu
 
 # Tags
-repover="1.5.0"
+repover="1.5.1"
 reponame="geuebt-validate"
 
 dirpath="${CONDA_PREFIX}/geuebt-validate"
